3e66ccba237f291af85b19c417dcc7d116d000ed
Edward <devarandrade@gmail.com>
2020-05-13 20:14:06 -0300

    detailing JS

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ let vetor = [11,22,433,50];
 vetor[0] = 9000;
 //insere valor após ultimo elemento
 vetor[5] = 7;
+let x = null;
+let y = 1;
+y = null;
+//undefined:este tipo de dado aparece quando criamos uma variavel
+// e tentamos acessar seu valor antede ter atribuido algo a ela.
+let x;
+console.log(x);
+// undefined é diferente de null
+console.log (null === undefined);
+//resumidamente, isto ocorre pois NULL ainda é um valor e UNDEFINED é quando JS 
+//nao sabe qual o tippo de dado.
