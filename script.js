fc2ce5e37ffca6fc04e2041e5d9dfbd2a71d13db
Edward <devarandrade@gmail.com>
2020-05-13 20:23:08 -0300

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("HELLO WORLD")
 var texto = "200";
-let vetor = [1,2;3;4;5,0,100];
+let vetor = [1,2,3,4,5,0,100];
 let vetor = [1,"hello world",true,[1,2,40]];
 let vetor = [11,22,433,50];
 //altera o valor da primeira posição
