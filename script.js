c783a5b7ace390a26ee52f03354befcfad448bf6
Edward <devarandrade@gmail.com>
2020-05-19 12:40:36 -0300

    saving

--- script.js
+++ script.js
@@ -18,3 +18,12 @@ console.log(x);
 console.log (null === undefined);
 //resumidamente, isto ocorre pois NULL ainda é um valor e UNDEFINED é quando JS 
 //nao sabe qual o tippo de dado.
+//funçoes: as funcoes sao utilizadas para criarmos uma sequencia de operaçoes
+//para serem executadas^.
+let helloworld = function(){
+    console.log("hello world");
+    console.log("hello world again");
+    console.log("hello world once again");
+}
+helloworld();
+//note que para fazer a funcao executar, vc precisa chama-la com os parenteses.
