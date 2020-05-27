b575210ebf331fa35ee1585f929ab73bf956828b
Edward <devarandrade@gmail.com>
2020-05-27 13:01:03 -0300

    challenge error

--- script.js
+++ script.js
@@ -62,3 +62,66 @@ somar(99,1);
 let resultado = somar(10,10);
 // retur faz com que a funcao seja uma ferramenta exelente para tornarmos
 //nosso codigo mais simples de entender
+// Este objeto não precisa ser mexido
+let carro = {
+    rodas: 4,
+    portas: 2,
+    cor: "cinza",
+    ano: 2012,
+    estado: "novo"
+};
+
+
+function mudarPropriedade(nomePropriedade, novoValor){
+    switch(nomePropriedade){
+        // O case rodas é um exemplo para as próximas partes
+        case "rodas":
+            if(novoValor > 1){
+                carro.rodas = novoValor;    
+            }
+            else{
+                return "Minimo de rodas precisa ser 2";
+            }
+            break;
+        case "portas":
+            // Para alterar as portas, é necessário que o
+            // novo valor seja maior que 0
+            // senão deve retornar a string "Minimo de portas precisa ser 1"
+            if(novovalor > 0){
+                carro.portas = novovalor;
+            }else{
+                return "Minimo de portas precisa ser 1"
+            }
+            break;
+        case "cor":
+            // para alterar a cor, novo valor pode ser igual a cinza ou preto ou vermelho
+            // se for diferente, retornar "Cor invalida"
+        if(novovalor = "cinza" || novovalor == "preto" || novovalor== "vermelho"){
+        carro.cor = novovalor;
+    }else{
+    }return "cor invalida";
+     break;
+        // criar um case para o "ano"
+        // alterar o ano do carro para o ano informado
+        // se o ano for igual a 2019, alterar o estado para "novo" 
+        // senao alterar o estado para "usado"
+        case "ano":
+            carro.ano = novoValor
+            if(novoValor == 2019){
+                carro.estado = "novo"
+            }else{ carro.estado = "usado";
+            }
+            break;
+            
+            
+       default:
+       
+            // retornar a fraase "Propriedade invalida";
+    return"Propriedade invalida";
+    
+    // retornar a frase Alteracao concluida somente se foi possivel
+    // alterar um dado do objeto.
+    }
+    return "Alteracao concluida";
+}
+//continuando... loops
